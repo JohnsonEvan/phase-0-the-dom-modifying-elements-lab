fdec08fcc44e9fc5acaf584cf56ca6938ba7f75a
Evan <evan.johnson99@yahoo.com>
2021-12-29 17:18:00 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.setAttribute('id','victory');
+
+newHeader.textContent = 'Evan is the champion';
+
+document.body.append('h1');
